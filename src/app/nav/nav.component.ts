46e4735e9e1fb9a6049522439a2a7dcaf14d8644
mriduava <mriduava@gmail.com>
2019-11-10 18:06:18 +0100

    need to fix bug

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, NgZone } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { StoryblokService } from '../storyblok.service';
-import { StoreService } from '../store.service';
+import { Component, OnInit, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+
 
 export interface IWindow extends Window {
   webkitSpeechRecognition: any;
@@ -9,139 +7,27 @@ export interface IWindow extends Window {
 
 @Component({
   selector: 'app-nav',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
 
-  constructor(private zone: NgZone, private router: Router, private store: StoreService,
-        private route: ActivatedRoute, private productData: StoryblokService) { }
-
-  // recStart(event){
-  //   //console.log('Hi');
-  // };
-
-  products: any = [];
-  product: any = [];
-  cartItem: any = [];
-  textArr: any = [];
+  constructor(private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit() {
-    const playAudio = () => {
-      let audio = new Audio();
-      audio.src = "./assets/bleep.wav";
-      audio.load();
-      audio.play();
-    }
-    
-    /**
-     * Get data from the Service
-     * Data will stored in the products array.
-     */
-    this.productData.getStory('/', {version: 'draft', starts_with: 'men/'})
-      .then(data => {
-        this.products = data.stories;    
-    });
 
-    const productId = this.route.snapshot.params.id;
-    this.productData.getStory(productId, {version: 'draft'})
-    .then(data => {
-      this.product = data.story.content;
-      //this.cartItem = data.story;
-      
-      // TO NERRATE PRODUCT DETAILS
-      this.textArr.push(`${this.product.title} has been selected, 
-                         ${this.product.summary} ...
-                         It's price ${this.product.price} SEK ... ...
-                         To Buy the Product,
-                         Please press "Control", and then say "Buy". ... ...
-                         To go to Product page,
-                         Please press "Control", and then say "Continue". ... ...`);
-    });
- 
-     // ADD TO CART
-     const addToCart = () => {
-       this.store.cartData.push(this.cartItem);
-       localStorage.setItem('products', JSON.stringify(this.cartItem))
-     }
-    
-    const goProduct = () => {
-      this.zone.run(() => this.router.navigateByUrl('/products'))
-     // recognition.stop()
-    }
-
-    const goToCart = () => {
-      this.zone.run(() => this.router.navigateByUrl('/cart'))
-      speechSynthesis.cancel();
-    }
-
-     /**
-      * Function to take voice command from user and if the command match 
-      * with the title of the product, it will navigate to that details page. 
-      * @param cmd Voice Command as String
-      */
-     const filterProduct = (cmd: string)=>{
-      this.products.filter(product=>{     
-         if(product.content.title.toLowerCase() == cmd.toLowerCase()){
-           speechSynthesis.cancel();
-           this.zone.run(() => this.router.navigate(['/products', product.id], { relativeTo: this.route }))
-         };
-       })
-     }
-
-     // SPEECH TO TEXT    
-    const {webkitSpeechRecognition} : IWindow = <IWindow>window;
-    const recognition = new webkitSpeechRecognition();
-    var SpeechGrammarList = SpeechGrammarList || window['webkitSpeechGrammarList'];
-    
-    var grammar = '#JSGF V1.0;'
-    var speechRecognitionList = new SpeechGrammarList();
-    speechRecognitionList.addFromString(grammar, 1);
-    recognition.grammars = speechRecognitionList;
-    recognition.lang = 'en-US';
-    recognition.continuous = false;
-    recognition.interimResults = false;
-    recognition.onresult = function(event) {
-        let last = event.results.length - 1;
-        let command = event.results[last][0].transcript;
-        console.log(command);  
-        
-        filterProduct(command); 
-        if(command.toLowerCase() === 'product'){  
-          speechSynthesis.cancel();
-          goProduct();          
-        }else if(command.toLowerCase() === 'bye'){ 
-          speechSynthesis.cancel(); 
-          addToCart();
-          goToCart();      
-        }            
-    };
-
-
-    var circle = document.getElementById("circle");
-    function once() {
-     // playAudio();
-      recognition.start();
-      circle.removeEventListener("click", once);
-      //let clcikAgain = circle.addEventListener('click', once);
-    }
-    circle.addEventListener("click", once);
-    setInterval(once, 10000)
-    
-    // this.recStart= (event) => {
-    //   //recognition.start();
-    //   goProduct();
+    // var circle = document.getElementById("circle");
+    // function once() {
+    //  // playAudio();
+    //   recognition.start();
+    //   circle.removeEventListener("click", once);
     // }
-
-    recognition.onspeechend = function() {
-        recognition.stop();
-    };
-    recognition.onerror = function(event) {
-      console.log(event.error);
-    }   
+    // circle.addEventListener("click", once);
+   // setInterval(once, 10000)
     
-    
+  
    
   }
 
